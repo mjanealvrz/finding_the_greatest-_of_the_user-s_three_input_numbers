a5e575bbc6a03fb27912759be25a4e48c2dd271c
MJ Gillado <mjgillado0@gmail.com>
2024-01-07 02:14:05 +0800

    Created pseudocode

--- determing_largest_number_from_three_input_numbers.py
+++ determing_largest_number_from_three_input_numbers.py
@@ -1,2 +1,14 @@
 # Program 3
 # Ask user to input 3 numbers. Find and print the greatest number using only if-else statement
+
+# pseudocode
+
+# -Ask the user to input first number
+# -Verify if the input is a valid number
+# -Ask the user to input second number
+# -Verify if the input is a valid number
+# -Ask the user to input the third number
+# -Verify if the input is a valid number
+# -Compare the numbers to find the greatest number
+# -Print the greatest number
+
