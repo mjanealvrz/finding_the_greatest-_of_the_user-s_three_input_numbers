24825575888b8a9b158c6a374d9550c377b82156
MJ Gillado <mjgillado0@gmail.com>
2024-01-07 04:18:17 +0800

    Added comparison for three numbers using elif and if-else statement

--- determing_largest_number_from_three_input_numbers.py
+++ determing_largest_number_from_three_input_numbers.py
@@ -6,18 +6,18 @@
 # -Ask the user to input first number
 first_input_number = int(input("\nInput first number: "))
 
-# -Verify if the input is a valid number
-
 # -Ask the user to input second number
 second_input_number = int(input("\nInput second number: "))
-# -Verify if the input is a valid number
 
 # -Ask the user to input the third number
 third_input_number = int(input("\nInput third number: "))
 
-# -Verify if the input is a valid number
-
 # -Compare the numbers to find the greatest number
+if first_input_number >= second_input_number and first_input_number >= third_input_number:
+    greatest_number = first_input_number
+elif second_input_number >= first_input_number and third_input_number >= second_input_number:
+    greatest_number = second_input_number
+else: greatest_number = third_input_number 
 
 # -Print the greatest number
 
