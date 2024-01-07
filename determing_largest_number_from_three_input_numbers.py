689e3193ee647a804612cb9a7775c6fa031d2801
MJ Gillado <mjgillado0@gmail.com>
2024-01-08 01:11:05 +0800

    Enter button

--- determing_largest_number_from_three_input_numbers.py
+++ determing_largest_number_from_three_input_numbers.py
@@ -38,6 +38,9 @@ background_image = tk.PhotoImage(file="Numeria.png")
 background_label = tk.Label(window, image=background_image)
 background_label.place(relwidth=1, relheight=1)
 
+enter_button = Ctk.CTkButton(window, text="Click to Enter",  fg_color="#009E60",border_color="#4F7942", width=10,border_width= 5, command=enter_button)
+enter_button.place(relx=0.5, rely=0.7, anchor="center")
+
 window.mainloop()
 
 
