d04d29165e5d915b96fec592a9705caf57fcc0c7
MJ Gillado <mjgillado0@gmail.com>
2024-01-08 23:04:53 +0800

    Revised code

--- determing_largest_number_from_three_input_numbers.py
+++ determing_largest_number_from_three_input_numbers.py
@@ -6,59 +6,66 @@
 
 # Using tkinter and customtkinter for the program's module
 import tkinter as tk
-from tkinter import Image
+from tkinter import PhotoImage
 import customtkinter as Ctk
-
 
 
 def finding_greatest_number():
     global result_label
     def find_greatest():
-        try:
-            num1_entry = int(input_num1_entry.get())
-            num2_entry = int(input_num2_entry.get())
-            num3_entry = int(input_num3_entry.get())
 
-            if not (num1_entry and num2_entry and num3_entry): 
+        # retrieve input values in the entry fields
+        # convert input values to integers
+        try:
+            number1_entry = int(input_number1_entry.get())
+            number2_entry = int(input_number2_entry.get())
+            number3_entry = int(input_number3_entry.get())
+
+            # if the fields are empty it will display error
+            if not (number1_entry and number2_entry and number3_entry): 
                 result_label.configure(text="Error: Please fill in all fields!")
             else:
-                    greatest = int( max(num1_entry, num2_entry, num3_entry))
+                    # find the maximum value 
+                    greatest = int( max(number1_entry, number2_entry, number3_entry))
+                    # display the greatest result
                     result_label.configure(text=f"Greatest number: {greatest}")
-                    
+
+         # display the error in the result label            
         except ValueError:
             result_label.configure(text="Error: Please enter valid numbers!")
             
-
-                        
-# Another window
+# Create  window
     win = Ctk.CTk()
     win.title("Input three number")
     win.geometry("600x600")
         
-# Entry widgets for user's input three numbers
+# make GUI components (button, entry fields, and labels).
+    # specify where GUI items should be placed in the window
+    # attach the method find_greatest() to the button.
+    
     frame = Ctk.CTkFrame(win, width=900, height=500, fg_color="transparent",border_width=5, border_color="#4F7942", bg_color= "transparent", corner_radius=50)
     frame.place(relx=0.5, rely=0.5, anchor= "center")
 
-    input_num1_label = Ctk.CTkLabel(frame, text="1st input:", width=10)
-    input_num1_label.place(relx=0.2, rely=0.3, anchor= "center")
-    input_num1_entry = Ctk.CTkEntry(frame )
-    input_num1_entry .place(relx=0.4, rely=0.3, anchor="center")
+    input_number1_label = Ctk.CTkLabel(frame, text="1st input:", width=10)
+    input_number1_label.place(relx=0.2, rely=0.3, anchor= "center")
+    input_number1_entry = Ctk.CTkEntry(frame )
+    input_number1_entry .place(relx=0.4, rely=0.3, anchor="center")
 
-    input_num2_label = Ctk.CTkLabel(frame, text="2nd input:", width=10)
-    input_num2_label.place(relx=0.2, rely=0.4, anchor= "center")
-    input_num2_entry = Ctk.CTkEntry(frame )
-    input_num2_entry .place(relx=0.4, rely=0.4, anchor="center")
+    input_number2_label = Ctk.CTkLabel(frame, text="2nd input:", width=10)
+    input_number2_label.place(relx=0.2, rely=0.4, anchor= "center")
+    input_number2_entry = Ctk.CTkEntry(frame )
+    input_number2_entry .place(relx=0.4, rely=0.4, anchor="center")
 
-    input_num3_label = Ctk.CTkLabel(frame, text="3rd input:", width=10)
-    input_num3_label.place(relx=0.2, rely=0.5, anchor= "center")
-    input_num3_entry = Ctk.CTkEntry(frame )
-    input_num3_entry .place(relx=0.4, rely=0.5, anchor="center")
+    input_number3_label = Ctk.CTkLabel(frame, text="3rd input:", width=10)
+    input_number3_label.place(relx=0.2, rely=0.5, anchor= "center")
+    input_number3_entry = Ctk.CTkEntry(frame )
+    input_number3_entry .place(relx=0.4, rely=0.5, anchor="center")
 
     find_button = Ctk.CTkButton(frame, text= "Find Greatest!",  fg_color="#009E60", border_color="#4F7942", width=10,border_width= 5, command=find_greatest)
     find_button.place(relx=0.5, rely=0.7, anchor="center")
 
 
-    result_label = Ctk.CTkLabel(frame, text="Greatest number: ", width=20)
+    result_label = Ctk.CTkLabel(frame, text="The greatest number is  . ", width=20)
     result_label.place(relx=0.5, rely=0.6)
 
 
@@ -90,9 +97,6 @@ window.mainloop()
 
 # Destroy window
 window.destroy()
-
-
-
 
 
 
